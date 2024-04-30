bc714db44a99d2b10747d993bd2ec477d87d68ac
EmidioDalzotto <ecdm20121998@hotmail.com>
2024-04-29 22:24:54 -0300

    adicionadas interações em ambiente virtual

--- helloworld.py
+++ helloworld.py
@@ -8,14 +8,41 @@ Como usar:
 Tenha a variável LANG devidamente configurada ex:
 export LANG=pt_BR
 
+ou informe através do CLI argument '--lang'
+assim como '--count=número'
+ou o usuário terá de digitar
 """
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __author___ = "tal de tal"
 __license__ = "unlicense"
 
 import os
+import sys
 
-current_language = os.getenv("LANG", "en_US")[:5]
+arguments = {
+    "lang": None,
+    "count": None,
+}
+for arg in sys.argv[1:]:
+    # TODO: TRATAR ValueError
+    key, value = arg.split("=")
+    key = key.lstrip("-").strip()
+    value = value.strip()
+    if key not in arguments:
+        print("Invalid Option `{key}`")
+        sys.exit()
+    arguments[key] = value
+
+current_language = arguments["lang"]
+if current_language is None:
+    # TODO: Usar Repetição
+    if "LANG" in os.environ: 
+        current_language = os.getenv("LANG")
+    else:
+        current_language = input("Choose a language:")
+
+
+current_language = current_language[:5]
 
 msg = {
     "en_US": "Hello, World!",
@@ -25,5 +52,7 @@ msg = {
     "fr_FR": "Bonjour, Monde!",
 }
 
-print (msg[current_language])
+print (
+    msg[current_language] * int(arguments["count"])
+  )
 
